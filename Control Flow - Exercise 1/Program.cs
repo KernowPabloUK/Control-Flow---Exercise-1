d5ced29e3fa5f8f33be65442fc4e16b502c444bb
KernowPabloUK <PaulMorganUK1@outlook.com>
2022-10-08 16:21:16 +0100

    Add project files.

---
+++ Control Flow - Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Control_Flow___Exercise_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1- Write a program and ask the user to enter a number.
+            // The number should be between 1 to 10.
+            // If the user enters a valid number, display "Valid" on the console.
+            // Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+            {
+                Console.WriteLine("Please enter a number between 1 & 10...");
+
+                string userInput = Console.ReadLine();
+                int userNumber = Convert.ToInt32(userInput);
+
+                if (userNumber >=1 && userNumber <= 10)
+                    Console.WriteLine("Valid");
+                else
+                    Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
